3a2e37804035696f870e9fb2e8706a9d7b1f69cc
Anup Patankar <anantpatankar1@icloud.com>
2023-01-11 16:10:26 +0530

    Node js

---
+++ nodejs/index.js
@@ -0,0 +1,4 @@
+//jshint es version:6
+const fs = require("node:fs");
+
+fs.copyFileSync("file1.txt", "file2.txt");
---
+++ nodejs/node_npm/index.js
@@ -0,0 +1,5 @@
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+var sh = superheroes.random();
+var sv = supervillains.random();
+console.log(sh + " vs " + sv);
